b7c88a836ca78e24a55da71562af0261d57ec675
pcanaleta <p.canaleta14@gmail.com>
2022-01-16 21:43:55 +0100

    DB

--- LuppeBot/spiders/actes.py
+++ LuppeBot/spiders/actes.py
@@ -14,8 +14,8 @@ class ResidentialRecordsSpider(scrapy.Spider):
     name = 'actes'
     categoria = {}
 
-    start_urls = [l.strip() for l in open('listofurls.txt').readlines()]
-    #start_urls = ['https://www.fcf.cat/resultats/2022/futbol-11/tercera-catalana/grup-1/jornada-14']
+    #start_urls = [l.strip() for l in open('listofurls.txt').readlines()]
+    start_urls = ['https://www.fcf.cat/resultats/2022/futbol-11/tercera-catalana/grup-18/jornada-14']
 
     def parse(self, response):
         actes_page_links = response.css('td.p-5.resultats-w-resultat.tc a')
@@ -33,4 +33,14 @@ class ResidentialRecordsSpider(scrapy.Spider):
         item = Jugador()
         item['nom'] = response.xpath('//*[@class="m-0 fs-30 va-b bold"]/text()').extract_first()
         item['equip'] = response.xpath('//*[@class="mt-5 fs-20 va-t darkgrey italic"]/text()').extract_first()
+        item['jornades_lliga'] = response.xpath('//*[@class="axis"]//div/text()').extract_first()
+        item['convocat'] = response.xpath('//*[@class="bar teal"]//*[@class="percent"]/text()').extract_first()
+        item['titular'] = response.xpath('//*[@class="bar salmon"]//*[@class="percent"]/text()').extract_first()
+        item['suplent'] = response.xpath('//*[@class="bar peach"]//*[@class="percent"]/text()').extract_first()
+        item['jugats'] = response.xpath('//*[@class="bar lime"]//*[@class="percent"]/text()').extract_first()
+        item['gols'] = response.xpath('//div[@class="gol"]//div[@class="comptador"]/text()').extract_first()
+        item['gols_partit'] = response.css('span.fs-30::text').extract_first()
+        item['targetes_grogues'] = response.css('div.groga-s div::text').extract_first()
+        item['targetes_vermelles'] = response.css('div.vermella-s div::text').extract_first()
+        item['targetes_dobles_grogues'] = response.css('div.groga-vermella-s div.comptador::text').extract_first()
         yield item
